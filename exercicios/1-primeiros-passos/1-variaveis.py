3bdfa03f341176e2681d2109e7c4cfddf6682f51
Dannee2006 <161341079+Dannee2006@users.noreply.github.com>
2024-02-26 13:21:27 -0300

    Update 1-variaveis.py

--- exercicios/1-primeiros-passos/1-variaveis.py
+++ exercicios/1-primeiros-passos/1-variaveis.py
@@ -4,8 +4,19 @@
 # - E não podemos usar palavras reservadas do Python, como if, for, from, etc. 
 
 # Declare variáveis do tipo string
+cor = "preta"
+cor = "laranja"
+movel = "cadeira"
+movel = "mesa de centro"
 
 # Declare variáveis de cada tipo numérico
+meses_do_ano = 1
+meses_do_ano = 2
+meses_do_ano = 3
 
+decimal = 2435.00
 
 # Declare uma variável do tipo booleano
+
+Brasil = True
+PT = False
